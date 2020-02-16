5527b22cb36cad63248c8594f6a77acf028d8c6f
Raza Gill <raza.gill@outlook.com>
2020-02-16 10:52:13 +0100

    chore: add Brewfile

--- setup/brew.sh
+++ setup/brew.sh
@@ -1,29 +1,10 @@
 #!/usr/bin/env bash
 # Check for Homebrew and install if we don't have it
-source ~/.cnc/setup/setup_func.sh
+source $CNC_HOME/setup/echo_helper.sh
 
 if test ! $(which brew); then
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  err "Homebrew is not installed"
+  exit 1
 fi
 
-brew tap homebrew/versions
-brew tap homebrew/dupes
-brew update
-brew upgrade
-
-# Install packages
-require_brew zsh
-require_brew antigen 
-require_brew coreutils 
-require_brew git 
-require_brew tree 
-require_brew trash 
-require_brew wget 
-require_brew nvm 
-require_brew python 
-require_brew tmux 
-require_brew ssh-copy-id 
-require_brew fasd --with-default-names 
-require_brew gnu-sed --with-default-names
-require_brew grep --with-default-names
-require_brew neovim
+brew bundle -file=$CNC_HOME/setup/Brewfile --no-lock
